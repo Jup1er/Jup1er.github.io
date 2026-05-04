484785f0c81b037a3e58de867f5bc1d72ef2d67e
Jingxuan Li <jing.x.li@mail.mcgill.ca>
2026-05-05 01:30:18 +0800

    update config and permissions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jingxuanli.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
