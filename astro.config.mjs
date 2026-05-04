11e86927201610e6e36c6c2eb99fe9a035e41611
Jingxuan Li <jing.x.li@mail.mcgill.ca>
2026-05-05 01:43:53 +0800

    fix: set base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://jingxuanli.github.io',
+  // 1. 确保 site 结尾没有斜杠
+  site: 'https://jup1er.github.io', 
+  // 2. 添加 base 属性，必须以斜杠开头和结尾
+  base: '/jingxuanli.github.io/', 
   vite: {
     plugins: [tailwindcss()]
   }
