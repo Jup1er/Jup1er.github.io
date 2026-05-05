7901efa73835577b506214533b9f16f609dffcb2
Jingxuan Li <jing.x.li@mail.mcgill.ca>
2026-05-05 11:37:51 +0800

    update site config for new repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // 1. 确保 site 结尾没有斜杠
   site: 'https://jup1er.github.io', 
-  // 2. 添加 base 属性，必须以斜杠开头和结尾
-  base: '/jingxuanli.github.io/', 
   vite: {
     plugins: [tailwindcss()]
   }
